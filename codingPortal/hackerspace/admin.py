51fcf403104525b086023f1cee9582008beaf3aa
Joezer-Ivan <joezerivan@gmail.com>
2016-10-08 19:36:02 +0530

    Cleans up the code

--- codingPortal/hackerspace/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CodingQuestion
-# Register your models here.
-
-admin.site.register(CodingQuestion)
-# admin.site.register(User)
--- codingPortal/hackerspace/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HackerspaceConfig(AppConfig):
-    name = 'hackerspace'
